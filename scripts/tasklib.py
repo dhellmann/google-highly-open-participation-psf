2bcf45f4d91201263ea5d96ab81eebb61a5dcfd1
titus@idyll.org <titus@idyll.org@c3b20254-093d-0410-bf4f-d590cd53973e>
2007-12-07 11:32:18 +0000

    misc task fixes

---
+++ scripts/tasklib.py
@@ -0,0 +1,53 @@
+import os
+
+class Task:
+    def __init__(self, summary, descr, tags, owner):
+        self.summary = summary
+        self.descr = descr
+        self.tags = tags
+        self.owner = owner
+
+    def __repr__(self):
+        return "Task: '%s'" % (self.summary,)
+
+def load_task_from_file(filename):
+    global_d = {}
+    local_d = {}
+    
+    try:
+        execfile(filename, global_d, local_d)
+    except:
+        pass
+
+    if local_d.get('ignore', False):
+        print 'ignoring:', filename
+        return
+
+    try:
+        summary = local_d['summary'].strip()
+        description = local_d['description'].strip()
+        tags = set(local_d['tags'].split(','))
+        owner = local_d['owner'].strip()
+    except KeyError:
+        print 'ERROR loading info from file', filename
+        return None
+
+    task = Task(summary, description, tags, owner)
+    return task
+
+def gather_tasks(startdir):
+    tasks = set()
+    
+    startdir = os.path.abspath(startdir)
+
+    for (dirpath, dirnames, filenames) in os.walk('.'):
+        if '.svn' in dirnames:
+            dirnames.remove('.svn')
+        for filename in filenames:
+            if filename.endswith('.txt'):
+                fullpath = os.path.join(dirpath, filename)
+                task = load_task_from_file(fullpath)
+                if task:
+                    tasks.add(task)
+
+    return tasks
